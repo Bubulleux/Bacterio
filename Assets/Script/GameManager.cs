a146b2bde28beaa9f63b48971625c4a36a8fc3f0
Bubulle <bubulle-lol@yahoo.fr>
2020-05-02 10:08:21 +0200

    Ajout Player food enemy

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int maxFood;
+    public int maxEnemy;
+    [SerializeField]
+    private int FoodCount;
+    private int EnemyCount;
+    public GameObject Food;
+    public GameObject Enemy;
+    public Transform foods;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FoodCount = foods.childCount;
+        while(FoodCount < maxFood)
+        {
+            GameObject go = Instantiate(Food, new Vector3(Random.Range(-100f, 100f), 0f, Random.Range(-100f, 100f)), Quaternion.identity, foods);
+            go.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0f, 1f), 1f, 1f);
+            FoodCount++;
+                           
+        }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+    public int point;
+    public Text pointCounter;
+    void Update()
+    {
+        pointCounter.text = "Point : " + point;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Food")
+        {
+            Destroy(other.gameObject);
+            point++;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 _vel = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+        transform.position += _vel * Time.fixedDeltaTime * speed;
+    }
+
+    
+}
